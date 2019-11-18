a7502e2cd60b7722aefcfc4c1159da0378d839a5
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-11-18 16:02:54 +0530

    Update fbConfig.php

--- fbConfig.php
+++ fbConfig.php
@@ -7,8 +7,8 @@ require_once 'inc/facebook.php';
 /*
  * Configuration and setup FB API
  */
-$appId = '434412236903187'; 								//Facebook App ID(create this from 																				developer.facebook.com)
-$appSecret = 'ca559e2eb696ec48a4abba85add9a95f'; 			// Facebook App Secret
+$appId = '752842578476107'; 								//Facebook App ID(create this from 																				developer.facebook.com)
+$appSecret = 'de1066955df7feab239265eabada59aa'; 			// Facebook App Secret
 $redirectURL = 'http://fb.chasingpapers.com/'; // Callback URL i.e. page where you want to redirect after 														login, this should be same as oauth login in dashboard
 //$redirectURL = 'http://cp.chasingpapers.com/'; // Callback URL
 //$redirectURL = 'http://cp4.chasingpapers.com/';
